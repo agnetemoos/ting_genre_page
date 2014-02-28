12bf0df7215fca244fd4baf074c9384fc5f07a99
holt83 <simonholt83@gmail.com>
2014-02-28 01:48:52 -0800

    Add hook theme implementation and templates

---
+++ templates/ting-genre-page-related-content.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Default HTML for displaying related content on a ting genre page.
+ *
+ * Available variables:
+ *  - $events
+ *    An array of related events (node objects) for the genre page.
+ *  - $news
+ *    An arary of related news (node objects) for the genre page.
+ *  - $groups
+ *    A render array containing each group of node objects to be displayed as
+ *    related content on the genre page.
+ */
+
+?>
+
+<?php if ($rows)
+
