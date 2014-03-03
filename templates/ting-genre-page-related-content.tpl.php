b53bf4c4271507a043d830404778d24eb6ec53db
holt83 <simonholt83@gmail.com>
2014-03-03 04:49:32 -0800

    Updates to theming

--- templates/ting-genre-page-related-content.tpl.php
+++ templates/ting-genre-page-related-content.tpl.php
@@ -8,18 +8,26 @@
  *    An array of related events (node objects) for the genre page.
  *  - $news
  *    An arary of related news (node objects) for the genre page.
- *  - $groups
- *    A render array containing each group of node objects to be displayed as
+ *  - $rows
+ *    A render array containing each row of node objects to be displayed as
  *    related content on the genre page.
  */
 
 ?>
 
-<?php if (TRUE): ?>
-	<div id="ting-genre-page-related-content">
-		<div class="group-blocks-inner group-blocks-four">
-			<p>En lille test...</p>
-		</div>
+<?php if ($rows): ?>
+	<div id="ting-genre-page-related-content" class="group-blocks--wrapper">
+		<?php foreach ($rows as $row): ?>
+			<div class="group-blocks--inner group-blocks--four">
+				<?php foreach($row['columns'] as $column_class => $column): ?>
+					<div class="<?php print $column_class; ?>">
+						<a href="<?php print $column['url']; ?>">
+							<h2><?php print $column['title']; ?></h2>
+						</a>
+					</div>
+				<?php endforeach; ?>
+			</div>
+		<?php endforeach; ?>
 	</div>
 <?php endif; ?>
 
